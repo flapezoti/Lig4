edad13904e166e0123d6e5c76e38117453307fea
Flavia Pezoti <flaviapezoti@gmail.com>
2016-09-24 23:58:51 -0300

    added a function to discover wich collumn the player chose and add a piece to it

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 var rows = 6;
 var collumns = 7;
+var piece_size = 50;
+var gap = 0;
 
 var board = {
 	canvas : document.getElementById("myBoard"),
@@ -7,8 +9,8 @@ var board = {
 	startBoard: function() {
 		this.turn =	 0,
 		this.ctx = this.canvas.getContext("2d");
-		this.canvas.width = 500;
-		this.canvas.height = 400;
+		this.canvas.width = piece_size  * collumns + gap * (collumns - 1);
+		this.canvas.height = piece_size  * rows + gap  * (rows - 1);
 		this.positions = [];
 		for (i = 0; i < rows; i++) {
 			this.positions[i] = [];
@@ -18,13 +20,13 @@ var board = {
 	},
 
 	drawBoard: function() {
-		var posx = 30;
-		var posy = 25;	
+		var posx = piece_size/2;
+		var posy = piece_size/2;	
 		for (i = 0; i < rows; i++){
 			for ( j = 0; j < collumns; j++){
 				this.ctx.beginPath();
-				this.ctx.arc(posx + (j  * 60), posy + ( posy + i * 60), 25, 0, Math.PI*2, false);
-				this.ctx.FillStyle = "#000000";
+				this.ctx.arc(posx + (j  * (piece_size+gap)), posy + ( i * (piece_size+gap)), 25, 0, Math.PI*2, false);
+				this.ctx.fillStyle = "#000000";
 				this.ctx.fill();
 				this.ctx.closePath();
 			}
@@ -41,8 +43,49 @@ var board = {
 	}
 }
 
+
+
 function startGame(){
 	board.startBoard();
 }
 
+function getMouseXPos(evt) {
+    var rect = board.canvas.getBoundingClientRect();
+    x = evt.clientX - rect.left;
+    return x;
+}
+
+function getChosenCollumn (mousepos){ //mudar para dowhile
+	for ( i = 0; i < collumns; i ++){
+		if ( mousepos >= (i * piece_size) && mousepos < ( i * piece_size + piece_size)) {
+			var collumn = i;
+		}
+	}
+	console.log(collumn);
+	return collumn;
+}
+
+function addPiece(collumn){ 
+	/*achar a linha correta, identificar se a coluna já está cheia,
+		se estiver cheia pedir outra coluna,
+		senão marcar a pedra na linha e coluna
+	*/
+	collumnpos = collumn * piece_size;
+	console.log(collumnpos);
+	board.ctx.clearRect(collumnpos, 0, piece_size, piece_size);
+	board.ctx.beginPath();	
+	board.ctx.arc(25 + collumn  * (piece_size+gap), 25 , piece_size/2, 0, Math.PI*2, false);
+	board.ctx.fillStyle = "#0000ff";
+	board.ctx.fill();
+	board.ctx.closePath();
+
+}
+
 startGame();
+board.canvas.addEventListener('mouseup', function (event) {
+	var mousepos = getMouseXPos(event);
+	chosenCollumn = getChosenCollumn(mousepos);
+	addPiece (chosenCollumn);
+});
+
+
